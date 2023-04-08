ca2439cfe87be7df182dcbbba00029265f92b1be
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-04-08 23:12:33 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,62 +2,68 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T, int size>
-class TPQueue {
- private:
-    T* arr;
-    int begin;
-    int end;
-    int count;
-
- public:
-    TPQueue() : begin(0), end(0), count(0) {
-        arr = new T[size];
-    }
-    ~TPQueue() {
-        delete[] arr;
-    }
-    void push(const T& item) {
-        if (count < size) {
-            if (count == 0) {
-                arr[end++] = item;
-                count++;
-            } else {
-                int i = end - 1;
-                bool pr = false;
-                while (i >= begin && item.prior > arr[i].prior) {
-                    arr[i + 1] = arr[i];
-                    i--;
-                    pr = true;
-                }
-                if (pr) {
-                    arr[i + 1] = item;
-                    end++;
-                } else {
-                    arr[--begin] = item;
-                }
-                count++;
-            }
-            if (end == size) end = 0;
-        }
-    }
-    T pop() {
-        T item = arr[begin++];
-        count--;
-        if (begin == size) begin = 0;
-        return item;
-    }
-    bool isEmpty() const {
-        return count == 0;
-    }
-    bool isFull() const {
-        return count == size;
-    }
-};
+#include <cstring>
+#include <stdexcept>
 
 struct SYM {
   char ch;
   int prior;
 };
+
+template<typename T, int size>
+class TPQueue {
+ private:
+    T* arr;
+    int begin, end, count;
+    int next(int current) {
+        return (current + 1) % size;
+    }
+
+    int prev(int current) {
+        return (current - 1 + size) % size;
+    }
+
+ public:
+    TPQueue(): begin(0), end(0), count(0), arr(new T[size]) {}
+
+    ~TPQueue() {
+        delete[] arr;
+    }
+
+    void push(const T& item) {
+        if (count == size) {
+            throw std::out_of_range("Queue is full");
+        }
+
+        if (count == 0) {
+            arr[begin] = item;
+            count++;
+            return;
+        }
+
+        int i = end;
+        while (i != begin && item.prior > arr[prev(i)].prior) {
+            arr[i] = arr[prev(i)];
+            i = prev(i);
+        }
+
+        arr[i] = item;
+        end = next(end);
+        count++;
+    }
+
+    T pop() {
+        if (count == 0) {
+            throw std::out_of_range("Queue is empty");
+        }
+
+        T item = arr[begin];
+        begin = next(begin);
+        count--;
+
+        return item;
+    }
+};
+
 
 #endif  // INCLUDE_TPQUEUE_H_
